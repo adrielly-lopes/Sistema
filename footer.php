0117e095e31a0980cc3c086a77bcbae342d78abe
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-08-09 11:04:02 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+        <p>
+            E-mail: fmap@corumba.ms.gov.br |
+            Telefone: (67) 3907-5342 |
+            Endereço: Rua Cuiabá, 1614, Dom Bosco
+        </p>
+        <p>
+            <a href="https://www.facebook.com/fundacaodemeioambientedopantanal/"> Facebook </a>
+        </p>
+        <p>
+            <a href="https://www.imasul.ms.gov.br/"> Imasul </a>
+        </p>
+        <p>
+            <a href="https://www.corumba.ms.gov.br/"> Prefeitura Municipal de Corumbá </a>
+        </p>
+        <p>
+            Copyright © 2021 FMAP | Corumbá MS
+        </p>
+</footer>
