964efc2aae818c52c7ce1766e026e78d491711db
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-08-27 12:17:50 -0400

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,30 +1,16 @@
-<?php 
+<?php
+include_once('conexao.php');
 
-    require_once 'classe.php';
-    $u = new usuario();
+$Login = $_POST['login'];
+$Senha = $_POST['senha'];
 
-    if(isset($_POST['login'])){
-        $login = addslashes($_POST['login']);
-        $senha = addslashes($_POST['senha']);
+mysqli_select_db($conn, '$database');
+$sql = "INSERT INTO usuarios (Login, Senha) VALUES ('$Login', '$Senha')";
 
-        if(!empty($login) && !empty($senha)){
-            $u->conectar('sistemafmap','localhost','root',''); 
-            echo "$msg";
-            if($u->msg == ""){
-                if($u->cadastrar($login,$senha)){
-                    echo "<script language='javascript' type='text/javascript'>alert('O usuario foi cadastrado com sucesso!')</script>";
-                    echo "<script language='javascript' type='text/javascript'>window.location.href='login.html'</script>";
-                }else{
-                    echo "<script language='javascript' type='text/javascript'>alert('O usuario já está cadastrado no sistema!')</script>";
-                    echo "<script language='javascript' type='text/javascript'>window.location.href='cadastro.html';</script>";
-                }
-            }else{
-                echo "Erro: ".$u->msg;
-            }
-        }else {
-            echo "<script language='javascript' type='text/javascript'>alert('Preencha todos os campos!')</script>";
-            echo "<script language='javascript' type='text/javascript'>window.location.href='cadastro.html';</script>";
-        }
-    }
-
+if(mysqli_query($conn, $sql)){
+    echo "Dados salvos com sucesso!";
+}
+else{
+    echo "Você já está cadastrado!";
+}
 ?>
