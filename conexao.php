272b7125e74c47205d4a60a7f1ecc45c508c9b55
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-08-27 12:18:19 -0400

    Update and rename classe.php to conexao.php

---
+++ conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "sistema fmap";
+
+$conn= mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+if(!$conn){
+    die("Erro de conexão: ".mysqli_connect_error());
+}
+echo "/nConexão bem sucedida";
+
+?> 
