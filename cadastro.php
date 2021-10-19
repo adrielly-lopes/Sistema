1b3b495256b6fa26fc9ecb8dcbb32bf82f6ad1ef
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-10-19 11:15:34 -0400

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,11 +1,13 @@
 <?php
 include_once('conexao.php');
 
-$Login = $_POST['login'];
-$Senha = $_POST['senha'];
+$Nome = $_POST['nome'];
+$Email = $_POST['email'];
+$Senha = md5($_POST['senha']);
+$Usuario = $_POST['usuario'];
 
 mysqli_select_db($conn, '$database');
-$sql = "INSERT INTO usuarios (Login, Senha) VALUES ('$Login', '$Senha')";
+$sql = "INSERT INTO usuario (Nome, Email, Senha, Usuario) VALUES ('$Nome', '$Email, '$Senha', '$Usuario')";
 
 if(mysqli_query($conn, $sql)){
     echo "Dados salvos com sucesso!";
