3e296dfc6816b1e0bf9932b80104772a038a7424
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-10-21 11:20:40 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,14 @@
-<?php 
+<?php
 include_once('conexao.php');
+
+mysqli_select_db($conn, '$database');
+$query = "SELECT Nome, Pass FROM usuarios"; 
+
+if(mysqli_query($conn, $query)){
+    echo "Vc conseguiu";
+}
+else{
+    echo "Não foi dessa vez";
+}
+
 ?>
