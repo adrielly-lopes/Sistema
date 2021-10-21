45d72d4cdbc7b195606c4cff2640e7fff0f512a0
adrielly-lopes <56943047+adrielly-lopes@users.noreply.github.com>
2021-10-21 12:36:15 -0400

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,10 @@
 include_once('conexao.php');
 
 mysqli_select_db($conn, '$database');
-$query = "SELECT Nome, Pass FROM usuarios"; 
+$query = "SELECT Nome, Pass, NivelAcesso FROM usuarios"; 
 
 if(mysqli_query($conn, $query)){
+    $NivelAcesso == 'servidor';
     echo "Vc conseguiu";
 }
 else{
